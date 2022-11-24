9ce9d57c67271e41c1ca649c296fabb9b5146c0f
slamacraft <1114951452@qq.com>
2022-11-24 17:14:22 +0800

    微信公众号子模块

---
+++ wechat/src/main/scala/org/example/app/wechat/WechatApplication.scala
@@ -0,0 +1,12 @@
+package org.example.app.wechat
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+
+@SpringBootApplication
+class WechatApplication
+
+object WechatApplication extends App {
+  SpringApplication.run(classOf[WechatApplication])
+}
