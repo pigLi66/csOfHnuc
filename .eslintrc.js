ccfbcbbf916ca7e93cd8925c167fe872fd3d3b61
slamacraft <1114951452@qq.com>
2023-03-09 09:01:12 +0800

    稍微更新一下

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+
+    root: true,
+
+    env: {
+
+        node: true,
+        'vue/setup-compiler-macros': true,
+
+    },
+
+    extends: [
+
+        "plugin:vue/vue3-essential",
+
+        "eslint:recommended",
+
+        "@vue/typescript/recommended",
+
+        // "@react-native-community"y
+
+    ],
+
+    parserOptions: {
+
+        ecmaVersion: 2020,
+
+    },
+
+    rules: {
+
+        "vue/no-mutating-props": "off",
+        "vue/multi-word-component-names": "off",
+        "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+
+        "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+
+        "no-empty-function": "off",
+        "@typescript-eslint/no-empty-function": "off",
+
+        "@typescript-eslint/no-var-requires": 0,
+
+        "@typescript-eslint/explicit-module-boundary-types": ["off"],
+
+        "@typescript-eslint/no-explicit-any": ["off"],
+
+        // "no-empty-pattern": true,
+
+    },
+
+};
