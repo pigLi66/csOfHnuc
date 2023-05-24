d96dc3073ee324822a149c6eb220f3ddc385f3af
slamacraft <1114951452@qq.com>
2023-05-24 15:18:22 +0800

    百度搜索支持已经上线

--- src/component/SearchEngine.vue
+++ src/component/SearchEngine.vue
@@ -1,32 +1,6 @@
-<!-- eslint-disable no-eval -->
-<template>
-    <div class="search">
-        <el-autocomplete placeholder="请输入内容" v-model="word" @keyup.enter="search()" class="search-input" style="width: 60%"
-            :fetch-suggestions="autoComplete" :popper-append-to-body='false' ref="input" @select="search()">
-            <template #prepend>
-                <el-select class="search-engine-select" v-model="searchEngines.select" placeholder="请选择"
-                    @change="searchEnginesChanged">
-                    <el-option v-for="item in searchEngines.options" :key="item.value" :label="item.label"
-                        :value="item.value">
-                        <span style="float: left">{{ item.label }}</span>
-                    </el-option>
-                </el-select>
-            </template>
-            <template #append>
-                <el-button class="search-button" @click="search()">
-                    <el-icon>
-                        <search />
-                    </el-icon>
-                </el-button>
-            </template>
-
-        </el-autocomplete>
-    </div>
-</template>
-
 <script lang="ts">
 // import md5 from "js-md5";
-import axios from "axios";
+// import axios from "axios";
 import { defineComponent } from "vue";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { ElMessage } from "element-plus";
@@ -35,23 +9,14 @@ import { ElMessage } from "element-plus";
 // import router from "@/router";
 import { Search } from "@element-plus/icons-vue";
 
-const defaultSuggestUrl =
-    "https://suggestion.baidu.com/su?wd=%word%&cb=window.baidu.sug";
-const defaultSuggestFunc =
-    "window.baidu={sug:function(json){cb(json.s.map(function(x){return{'value':x}}))}}";
-const api = {
-  searchEngines: "/search/searchEngines",
-  searchLog: "/search/searchLog",
-};
-
-type EngineOption = {
-    id: any,
-    value: any
-    mainUrl: any
-    suggestUrl: any
-    suggestFunc: any
-    icon: any
-    label: any
+interface EngineOption {
+  [key: string]: {
+    name: string;
+    mainUrl: string;
+    suggestUrl: string;
+    suggestFunc: any;
+    icon: string;
+  };
 }
 
 export default defineComponent({
@@ -65,96 +30,45 @@ export default defineComponent({
       word: "",
       searchIcon: "search-icon el-icon-search",
       searchEngines: {
-        select: "",
-        select_engine_id: 0,
-        main_url: "",
-        suggest_url: "",
-        suggest_func: "",
-        options: [] as EngineOption[],
+        currentEngines: "baidu",
+        options: {
+          baidu: {
+            name: "百度",
+            mainUrl:
+              "http://www.baidu.com/s?wd=%word%&rsv_spt=1&issp=1&rsv_bp=0&ie=utf-8&tn=baidulocal&inputT=2969",
+            suggestUrl:
+              "https://suggestion.baidu.com/su?wd=%word%&cb=window.baidu.sug",
+            suggestFunc:
+              "window.baidu = { sug: function (json) { callback(json.s.map(function (x) { return { 'value': x } })) } }",
+            icon: "",
+          },
+        } as EngineOption,
       },
     };
   },
   methods: {
-    searchEnginesChanged() {
-      for (let x = 0; x < this.searchEngines.options.length; x++) {
-        // eslint-disable-next-line eqeqeq
-        if (this.searchEngines.select == this.searchEngines.options[x].value) {
-          this.searchEngines.select_engine_id = this.searchEngines.options[
-            x
-          ].id;
-          this.searchEngines.main_url = this.searchEngines.options[x].mainUrl;
-          this.searchEngines.suggest_url = this.searchEngines.options[
-            x
-          ].suggestUrl;
-          this.searchEngines.suggest_func = this.searchEngines.options[
-            x
-          ].suggestFunc;
-          this.searchIcon = this.searchEngines.options[x].icon;
-          return;
-        }
-      }
-    },
-    async searchEnginesGet() {
-      try {
-        const { data: res } = await axios.get(api.searchEngines);
-        for (let s = 0; s < res.data.length; s++) {
-          const suggestUrl = defaultSuggestUrl;
-          const suggestFunc = defaultSuggestFunc;
-          // if (md5(res.data[s].suggest_func) == res.data[s].s) {
-          //     suggest_url = res.data[s].suggest_url;
-          //     suggest_func = res.data[s].suggest_func;
-          // }
-          this.searchEngines.options.push({
-            id: res.data[s].id,
-            mainUrl: res.data[s].main_url,
-            suggestUrl: suggestUrl,
-            suggestFunc: suggestFunc,
-            icon: res.data[s].icon,
-            label: res.data[s].name,
-            value: res.data[s].name,
-          });
-        }
-        this.searchEngines.select = this.searchEngines.options[0].value;
-        this.searchEngines.select_engine_id = this.searchEngines.options[0].id;
-        this.searchEngines.main_url = this.searchEngines.options[0].mainUrl;
-        this.searchEngines.suggest_url = this.searchEngines.options[0].suggestUrl;
-        this.searchEngines.suggest_func = this.searchEngines.options[0].suggestFunc;
-        this.searchIcon = this.searchEngines.options[0].icon;
-      } catch (e: any) {
-        console.log(e);
-        ElMessage.error(e.response.data.msg)
-      }
-    },
     async search() {
-      const searchUrl = this.searchEngines.main_url.replace("%word%", this.word);
+      const currentEnginesOption =
+        this.searchEngines.options[this.searchEngines.currentEngines];
+      const searchUrl = currentEnginesOption.mainUrl.replace(
+        "%word%",
+        this.word
+      );
       window.open(searchUrl);
       this.autoComplete("");
-      try {
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const { data: res } = await axios.post(api.searchLog, {
-          userId: this.userId,
-          engine_id: this.searchEngines.select_engine_id,
-          search_text: this.word,
-        });
-      } catch (e) {
-        console.log(e);
-      }
-      this.word = "";
     },
-    async autoComplete(queryString: any, cb: any = null) {
-      if (
-        queryString === "" ||
-                // queryString === [] ||
-                queryString === undefined
-      ) {
-        try {
-          // eslint-disable-next-line standard/no-callback-literal
-          cb([]);
-        } catch (e) {
-          console.log(e)
-        }
+
+    async autoComplete(
+      queryString: string,
+      callback?: (query: string[]) => void
+    ) {
+      if (!queryString && callback) {
+        // eslint-disable-next-line standard/no-callback-literal
+        callback([]);
       } else {
-        const autoCompleteUrl = this.searchEngines.suggest_url.replace(
+        const currentEnginesOption =
+          this.searchEngines.options[this.searchEngines.currentEngines];
+        const autoCompleteUrl = currentEnginesOption.suggestUrl.replace(
           "%word%",
           this.word
         );
@@ -162,59 +76,85 @@ export default defineComponent({
           const script = document.createElement("script");
           script.src = autoCompleteUrl;
           // eslint-disable-next-line no-eval
-          eval(this.searchEngines.suggest_func);
+          eval(currentEnginesOption.suggestFunc);
           document.querySelector("head")?.appendChild(script);
           document.querySelector("head")?.removeChild(script);
         } catch (e: any) {
           console.log(e);
-          ElMessage.error(e.response.data.msg)
+          ElMessage.error(e.response.data.msg);
         }
       }
     },
   },
-  created() {
-    this.searchEnginesGet();
-  },
-  mounted() {
-    // (this.$refs['input'] as FormInstance)
-    // (this.$refs['input'] as FormInstance).validate((isValid: boolean) => {
-    //     if (isValid) {
-    //         console.log(isValid)
-    //         loginApi.login(this.loginForm).then((res) => {
-    //             console.log(res.data)
-    //             store.state.token = res.data.token
-    //             router.push("/home")
-    //         })
-    //     }
-    // })
-    // this.$refs["input"].focus();
-  },
 });
 </script>
 
+<!-- eslint-disable no-eval -->
+<template>
+  <div class="search">
+    <el-autocomplete
+      clearable
+      placeholder="请输入内容"
+      v-model="word"
+      @keyup.enter="search()"
+      class="search-input"
+      style="width: 60%"
+      :fetch-suggestions="autoComplete"
+      :popper-append-to-body="false"
+      ref="input"
+      debounce="0"
+      @select="search()"
+    >
+      <template #prepend>
+        <el-select
+          class="search-engine-select"
+          v-model="searchEngines.currentEngines"
+          placeholder="请选择"
+        >
+          <el-option
+            v-for="(value, key) in searchEngines.options"
+            :key="key"
+            :label="value.name"
+            :value="key"
+          >
+            <span style="float: left">{{ value.name }}</span>
+          </el-option>
+        </el-select>
+      </template>
+      <template #append>
+        <el-button class="search-button" @click="search()">
+          <el-icon>
+            <search />
+          </el-icon>
+        </el-button>
+      </template>
+    </el-autocomplete>
+  </div>
+</template>
+
 <style scoped lang="scss">
 .search {
-    width: 100%;
-    //text-align: center;
-    horiz-align: center;
+  width: 100%;
+  //text-align: center;
+  horiz-align: center;
 
-    .search-input {
-        margin-left: 50px;
-        margin-right: 50px;
-        width: 70%;
-    }
+  .search-input {
+    margin-left: 50px;
+    margin-right: 50px;
+    width: 70%;
+  }
 }
 
 .search-icon-div {
-    padding-top: 80px;
-    padding-bottom: 60px;
+  padding-top: 80px;
+  padding-bottom: 60px;
 }
 
 .search-engine-select {
-    width: 100px;
+  width: 100px;
 }
 
 .search-button {
-    width: 70px;
+  width: 70px;
 }
 </style>
